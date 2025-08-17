b26115428a1b1742eac2b9ba6ea39631d7ec7026
Michael Okang <michaelokang8@gmail.com>
2025-08-17 13:08:03 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,5 @@
--- task_4.sql
--- Print full description of 'books' table without DESCRIBE/EXPLAIN/ANALYZE
+USE alx_book_store;
 
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
   AND TABLE_NAME = 'books';
