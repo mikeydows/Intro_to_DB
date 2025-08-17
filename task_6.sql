578a3c236f7728df990821cb27042337d568ebc8
Michael Okang <michaelokang8@gmail.com>
2025-08-17 13:32:15 +0000

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,8 @@
-INSERT INTO Customers(customer_id = 2, customer_name, email, address)
+INSERT INTO customer(customer_id = 2, customer_name, email, address)
 VALUES('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave."');
 
-INSERT INTO Customers(customer_id = 3, customer_name, email, address)
+INSERT INTO customer(customer_id = 3, customer_name, email, address)
 VALUES('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
-INSERT INTO Customers(customer_id = 4, customer_name, email, address)
+INSERT INTO customer(customer_id = 4, customer_name, email, address)
 VALUES('Nehemial Kamolu', 'nkamolu@sandtech.com','126 Happiness  Ave.');
